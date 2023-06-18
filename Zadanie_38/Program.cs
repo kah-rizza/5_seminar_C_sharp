41da0b5502b9f5b60b507b4aa8de29d012d831a5
kah-rizza <bu95509@gmail.com>
2023-06-19 02:31:46 +0600

    максимум и минимум

--- Zadanie_38/Program.cs
+++ Zadanie_38/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив вещественных чисел
+        double[] array = { 2.5, 1.3, 4.7, 3.1, 2.8 };
+        
+        // Инициализируем переменные для максимального и минимального элементов
+        double max = array[0];
+        double min = array[0];
+        
+        // Находим максимальный и минимальный элементы
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+            
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        
+        // Находим разницу между максимальным и минимальным элементами
+        double difference = max - min;
+        
+        // Выводим результат на экран
+        Console.WriteLine("Массив: " + string.Join(", ", array));
+        Console.WriteLine("Максимальный элемент: " + max);
+        Console.WriteLine("Минимальный элемент: " + min);
+        Console.WriteLine("Разница между максимальным и минимальным элементами: " + difference);
+    }
+}
